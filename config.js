4b866e6934ef576ac3b7fc189cb5bf6325b87ac1
GAPUNYASIRKEL <82222755+GAPUNYASIRKEL@users.noreply.github.com>
2025-03-10 19:25:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,14 @@ module.exports = {
   language: "en",
   ownerID: ["804957803529109505", ""], 
   mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId : "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret : "199a619d22dd4e55a4a2c1a7a3d70e63",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "Kenangan Mantan", 
-  activityType: "PLAYING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/MH9KECTJ",
+  activityName: "YouTube Music", 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
