74e089b410fac03e46b170f523cc53e81c63dde0
GAPUNYASIRKEL <82222755+GAPUNYASIRKEL@users.noreply.github.com>
2025-03-15 08:32:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,23 +4,23 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  mongodbUri : "mongodb+srv://shiva:shiva@discordbot.opd5w.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId : "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret : "199a619d22dd4e55a4a2c1a7a3d70e63",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/ahkMq8x",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "193.226.78.187",
-      port:  9372,
+      name: "Kaze",
+      password: "kazegantenk",
+      host: "192.99.10.133",
+      port:  9894,
       secure: false
     }
   ]
