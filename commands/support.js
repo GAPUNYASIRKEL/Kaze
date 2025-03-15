fbd97fb9572d825b127066a9728e42d8ec693f9b
GAPUNYASIRKEL <82222755+GAPUNYASIRKEL@users.noreply.github.com>
2025-03-15 08:43:14 +0700

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -9,10 +9,10 @@ module.exports = {
     options: [],
     run: async (client, interaction, lang) => {
         try {
-            const supportServerLink = "Bot From Kaze";
-            const githubLink = "Kaze";
-            const replitLink = "Kaze";
-            const youtubeLink = "Kaze";
+            const supportServerLink = "https://discord.gg/xQF9f9yUEM";
+            const githubLink = "https://github.com/GlaceYT";
+            const replitLink = "https://replit.com/@GlaceYT";
+            const youtubeLink = "https://www.youtube.com/@GlaceYT";
 
             const embed = new EmbedBuilder()
                 .setColor('#b300ff')
@@ -27,7 +27,7 @@ module.exports = {
                     .replace("{replitLink}", replitLink)
                     .replace("{youtubeLink}", youtubeLink)
                 )
-                .setImage('https://cdn.discordapp.com/attachments/847065469035085834/1350267061120794684/komi-san-wa-4k-wallpaper-3840x2160-uhdpaper.com-291.1_c.jpg?ex=67d61db2&is=67d4cc32&hm=2989c76b29d62455eaeb1967d60e8df640b90af6542be85f5a66a535586cf7bb&')
+                .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
                 .setTimestamp();
 
             await interaction.reply({ embeds: [embed] });
