57c32d9047df9992ddb4ddf580b4d4a80947133f
GAPUNYASIRKEL <82222755+GAPUNYASIRKEL@users.noreply.github.com>
2025-03-16 10:15:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,17 +10,17 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "Komi-San", 
+  activityName: "With Komi-San", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/ahkMq8x",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
-      name: "Kaze",
-      password: "kazegantenk",
-      host: "192.99.10.133",
-      port:  9894,
+      name: "Avinan",
+      password: "avinan",
+      host: "new-york-node-1.vortexcloud.xyz",
+      port:  9323,
       secure: false
     }
   ]
